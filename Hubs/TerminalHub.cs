6a9ac3c6eebcd1a2f341deb8644aec5f35ba2c7d
Emre Karahan <emre.karahan@gmail.com>
2016-08-25 07:30:06 +0300

    second

--- Hubs/TerminalHub.cs
+++ Hubs/TerminalHub.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Text;
-using System.Threading;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.SignalR.Hubs;
 using Microsoft.DotNet.InternalAbstractions;
@@ -12,21 +10,13 @@ namespace terminalApp.Hubs
     [HubName("terminalHub")]
     public class TerminalHub : Hub
     {
-
-        //private static  Dictionary<string, ProcessStartHelper> _processDictionary = new Dictionary<string, ProcessStartHelper>();
-
         private ProcessStartHelper _process = new ProcessStartHelper();
         public TerminalHub()
         {
-            //_processDictionary = 
-            //_process = new ProcessStartHelper();
         }
 
         public void Receive(string cmd, string connID)
         {
-
-            //var objectKey = $"obj_{connID}";
-
             _process.OutputDataReceived += (s, e) => Clients.Client(connID).message($"{e.Data}");
             _process.ErrorDataReceived += (s, e) => Clients.Client(connID).message($"{e.Data}");
             //_process.Exited += (s, e) => Clients.Client(connID).message($"Procees Finished...");
@@ -36,16 +26,7 @@ namespace terminalApp.Hubs
         }
 
         public override System.Threading.Tasks.Task OnConnected()
-        {
-
-            //var objectKey = $"obj_{Context.ConnectionId}";
-
-            // if (!_processDictionary.ContainsKey(objectKey))
-            // {
-            //     ProcessStartHelper process = new ProcessStartHelper();
-            //     _processDictionary.Add(objectKey, process);
-            // }
-
+        {    
             StringBuilder greetings = new StringBuilder();
             greetings.Append(RuntimeEnvironment.OperatingSystemPlatform);
             greetings.Append($" {System.Environment.MachineName}");
